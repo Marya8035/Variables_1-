3e57e77da904533d02e8e75d887234c24ef3f769
Marya8035 <macha.zyrina@gmail.com>
2024-10-06 14:53:31 +0500

    Add files via upload

---
+++ Variables_1.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+hours_spent = 1.5
+course_name = "Puthon"
+average_execution_time = hours_spent / total_tasks
+print(course_name, 'всего задач', total_tasks, 'затрачено часов', hours_spent, 'среднее время выполнения', average_execution_time)
